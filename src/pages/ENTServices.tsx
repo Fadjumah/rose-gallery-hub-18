e067855f6bd1f827d7a8b9b7cb35c99665fb8ac0
Fadjumah <Abalubega10@gmail.com>
2025-12-31 02:16:26 +0300

    Add Google Ads script to ENTServices page

--- src/pages/ENTServices.tsx
+++ src/pages/ENTServices.tsx
@@ -88,6 +88,8 @@ const ENTServices = () => {
       </Helmet>
 
       <div className="min-h-screen bg-background">
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1241677876393680"
+     crossorigin="anonymous"></script>
         <Header />
         
         {/* Hero Section */}
