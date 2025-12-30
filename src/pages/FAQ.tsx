bf2d139d4a5ae079cfe98c76e3b7d831c398a27f
Fadjumah <Abalubega10@gmail.com>
2025-12-31 02:17:10 +0300

    Add Google Ads script to FAQ page

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -19,6 +19,8 @@ const FAQ = () => {
       </Helmet>
 
       <div className="min-h-screen bg-background">
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1241677876393680"
+     crossorigin="anonymous"></script>
         <Header />
 
         {/* Hero Section */}
