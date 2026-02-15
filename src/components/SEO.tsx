3017465a81a084f90f133eca5cae2e124ed04bc0
Fadjumah <Abalubega10@gmail.com>
2026-02-15 05:07:43 +0300

    Update SEO.tsx

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -20,25 +20,22 @@ const SEO: React.FC<SEOProps> = ({
 }) => {
   return (
     <Helmet>
-      {/* Primary Meta */}
       <title>{title}</title>
       <meta name="description" content={description} />
+
       {canonical && <link rel="canonical" href={canonical} />}
 
-      {/* Open Graph / Facebook */}
       <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:url" content={url} />
       {image && <meta property="og:image" content={image} />}
 
-      {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       {image && <meta name="twitter:image" content={image} />}
 
-      {/* JSON-LD Schema */}
       {schema && (
         <script type="application/ld+json">{JSON.stringify(schema)}</script>
       )}
