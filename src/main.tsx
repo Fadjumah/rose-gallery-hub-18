7afcc3cbf7faee7ca5f6a4beecde11f7ea033410
Fadjumah <Abalubega10@gmail.com>
2026-02-15 05:05:43 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
+import "./index.css";
 
-// Create the root element and wrap App with HelmetProvider
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HelmetProvider>
